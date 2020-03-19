c5f042c5dc100f0d2d4edf95ee4d7f0bbcfad307
akkkr <aki112553@gmail.com>
2020-03-19 15:51:48 +0900

    First Commit

---
+++ github/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  github
+//
+//  Created by 倉谷　明希 on 2020/03/19.
+//  Copyright © 2020 aki kuratani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("h")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
